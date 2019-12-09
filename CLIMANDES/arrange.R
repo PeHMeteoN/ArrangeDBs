6066a6013054301793f47cb201a14a3ad08cebee
Cesar Luis Aybar Camacho <aybar1994@gmail.com>
2019-12-09 18:54:11 -0500

    first commit

---
+++ CLIMANDES/arrange.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+data_dir <- "DECADE_database_sh/DECADE_detabase/DECADE_data_final_ACMANT/"
+variables <- c("PRCP", "TN", "TX")
+
+
+# 1. Precipitation --------------------------------------------------------
+
+
